a6bbf85ef567a910ad27b33f0b14b18c9af759af
Amenable-C <duswo0624@naver.com>
2023-06-21 11:31:47 +0900

    5-3. Persistence Layer 테스트 (1)

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,31 @@
+package sample.cafekiosk.spring.domain.product;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import sample.cafekiosk.spring.domain.BaseEntity;
+
+@Getter
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingStatus sellingStatus;
+
+    private String name;
+
+    private int price;
+}
